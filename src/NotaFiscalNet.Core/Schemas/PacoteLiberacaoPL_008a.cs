35285b3316fca4fa50578b7ef6effeadadc51390
William Thiago <williamthiago@gmail.com>
2016-03-19 04:07:45 -0300

    Adicionado schemas da versão PL_008h2 + Evento de Carta de Correção

--- src/NotaFiscalNet.Core/Schemas/PacoteLiberacaoPL_008a.cs
+++ src/NotaFiscalNet.Core/Schemas/PacoteLiberacaoPL_008a.cs
@@ -3,32 +3,32 @@ using System.IO;
 
 namespace NotaFiscalNet.Core.Schemas
 {
-    public class PL008a : IPacoteLiberacaoNFe
+    public class PacoteLiberacaoPL_008a : IPacoteLiberacaoNFe
     {
-        private const string BASE_PATH = "Schemas\\PL_008a\\{0}";
-        private const string BASE_PATH_EVENTO_CANCELAMENTO = "Schemas\\Evento_Canc_PL\\{0}";
+        private const string BasePath = "Schemas\\PL_008a\\{0}";
+        private const string BasePathEventoCancelamento = "Schemas\\Evento_Canc_PL\\{0}";
 
         public string VersaoLayout => "3.10";
 
         public string PathNFe => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH, "nfe_v3.10.xsd"));
+            string.Format(BasePath, "nfe_v3.10.xsd"));
 
         public string PathNFeNoSig => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH, "nfe_v3.10_NoSig.xsd"));
+            string.Format(BasePath, "nfe_v3.10_NoSig.xsd"));
 
         public string PathEnviNFe => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH, "enviNFe_v3.10.xsd"));
+            string.Format(BasePath, "enviNFe_v3.10.xsd"));
 
         public string PathEnvEventoCancelamento => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH_EVENTO_CANCELAMENTO, "envEventoCancNFe_v1.00.xsd"));
+            string.Format(BasePathEventoCancelamento, "envEventoCancNFe_v1.00.xsd"));
 
         public string PathEnvEventoCancelamentoNoSig => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH_EVENTO_CANCELAMENTO, "envEventoCancNFe_v1.00_NoSig.xsd"));
+            string.Format(BasePathEventoCancelamento, "envEventoCancNFe_v1.00_NoSig.xsd"));
 
         public string PathInutNFe => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH, "inutNFe_v3.10.xsd"));
+            string.Format(BasePath, "inutNFe_v3.10.xsd"));
 
         public string PathInutNFeNoSig => Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-            String.Format(BASE_PATH, "inutNFe_v3.10_NoSig.xsd"));
+            string.Format(BasePath, "inutNFe_v3.10_NoSig.xsd"));
     }
 }
